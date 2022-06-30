110454c3b468c207131645e177b6b651d98e9458
baithicia <baithiciaa@gmail.com>
2022-06-30 13:55:43 -0700

    hello compile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-
 /* program to run puzzle  */
-
 int main(void)
 {
-
+	/* program to tun pazzle */
 	puts("\"Programming is like building a multilingual puzzle");
-	
 	return (0);
-
 }
