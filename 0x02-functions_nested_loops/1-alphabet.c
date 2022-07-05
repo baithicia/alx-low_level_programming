6269d8e6de775e0a8023c6b68ce7d14fd262aba9
baithicia <baithiciaa@gmail.com>
2022-07-05 14:10:13 -0700

    file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+nclude "main.h"                                                                 
+
+/*                                                                                
+
+ * print_alphabet - Check description                                             
+
+ * Description: It prints the alphabet in lowercase fallowed by a new line        
+
+ * Return: Nothing.                                                               
+
+ */                                                                               
+
+void print_alphabet(void)                                                         
+
+{                                                                                 
+
+char i;                                                                           
+
+for (i = 'a'; i <= 'z'; i++)                                                      
+
+_putchar(i);                                                                      
+
+_putchar('\n');                                                                   
+
+}                                                                                 
+
+int main(void)                                                                    
+
+{                                                                                 
+
+print_alphabet();                                                                 
+
+return (0);                                                                       
+
+}
