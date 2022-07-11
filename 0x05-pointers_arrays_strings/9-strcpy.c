b5807ad915c47ab2f848e3db53cefc0e6ee40230
baithicia <baithiciaa@gmail.com>
2022-07-11 15:06:33 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
-/**
-*strcpy - a function that copies the string pointed to by src,
+/** 9-strcpy.c
+*_strcpy - a function that copies the string pointed to by src,
 * including the terminating null byte (\0),
 * to the buffer pointed to by dest.
 * @dest: A pointer to destination of string
