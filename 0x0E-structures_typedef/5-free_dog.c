d12c99defd05daea4dd5a1c35efa225e0a46fae9
baithicia <baithiciaa@gmail.com>
2022-08-16 06:03:03 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+* free_dog - free the structure dog.
+* @d: pointer to the structure.
+**/
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
