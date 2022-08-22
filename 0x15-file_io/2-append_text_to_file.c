49dda834140dde920d3a12c99c96e72807057788
baithicia <baithiciaa@gmail.com>
2022-08-22 13:35:27 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,30 +1,27 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 /**
-* append_text_to_file - appends text at the end of a file.
-* @filename: name of the file to append the text.
-* @text_content: the NULL terminated string to add at the end of the file.
+* append_text_to_file - that appends text at the end of a file
+* @filename: variable pointer
+* @text_content: content file
+* Description: function that appends text at the end of a file
 * Return: 1 on success, -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int file_descriptor, length = 0;
-if (!filename)
+int i = 0, file;
+if (filename == NULL)
 return (-1);
-file_descriptor = open(filename, O_RDWR | O_APPEND);
-if (file_descriptor < 0)
-return (-1);
-if (!text_content)
+if (text_content == NULL)
+text_content = "";
+while (text_content[i] != '\0')
 {
-close(file_descriptor);
-return (1);
+i++;
 }
-while (text_content[length])
-length++;
-if (write(file_descriptor, text_content, length) < 0)
-{
-close(file_descriptor);
+file = open(filename, O_WRONLY | O_APPEND);
+if (file == -1)
 return (-1);
-}
-close(file_descriptor);
+write(file, text_content, i);
 return (1);
 }
